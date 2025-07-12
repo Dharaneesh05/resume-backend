6f34b2aae4495de907e467ea8eb687829b18e251
Dharaneesh <dharaneeshcs05@gmail.com>
2025-07-12 19:57:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -400,4 +400,5 @@ def export_excel():
         return jsonify({'status': 'error', 'message': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000, use_reloader=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
